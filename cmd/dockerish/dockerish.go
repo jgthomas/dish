6b8024abd0d557029b9667edf7fb928149923369
James Thomas <codeactor@protonmail.com>
2019-11-21 19:41:38 +0000

    Tidy up long lines

--- cmd/dockerish/dockerish.go
+++ cmd/dockerish/dockerish.go
@@ -11,6 +11,8 @@ import (
 )
 
 const runSelf = "/proc/self/exe"
+const rootfs = "/home/james/xenial-root"
+const root = "/"
 
 func main() {
 	switch os.Args[1] {
@@ -35,15 +37,19 @@ func run() {
 }
 
 func child() {
-	fmt.Printf("running %v as PID %d\n", os.Args[2:], os.Getpid())
+	fmt.Printf(
+		"running %v as PID %d\n",
+		os.Args[2:],
+		os.Getpid(),
+	)
 
 	cmd := exec.Command(os.Args[2], os.Args[3:]...)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 
-	util.Must(syscall.Chroot("/home/james/xenial-root"))
-	util.Must(os.Chdir("/"))
+	util.Must(syscall.Chroot(rootfs))
+	util.Must(os.Chdir(root))
 	util.Must(syscall.Mount("proc", "proc", "proc", 0, ""))
 	util.Must(cmd.Run())
 }
